16df6a8f28895f85b109691b17b463737af509aa
Zachary R <zacharywarnerricheson@gmail.com>
2022-11-10 17:33:02 -0600

    declare the motor controllers and add tank drive method

--- src/main/java/frc/robot/subsystems/MotorControllingSubsystem.java
+++ src/main/java/frc/robot/subsystems/MotorControllingSubsystem.java
@@ -1,17 +1,43 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
+// vanilla is the best ice cream.
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj.Joystick;
 
 public class MotorControllingSubsystem extends SubsystemBase {
+  private CANSparkMax m_frontLeft;
+  private CANSparkMax m_backLeft;
+  private CANSparkMax m_frontRight;
+  private CANSparkMax m_backRight;
+  private DifferentialDrive m_drive;
+  private Joystick m_leftStick; 
+  private Joystick m_rightStick;
   /** Creates a new MotorControllingSubsystem. */
-  public MotorControllingSubsystem() {}
-
+  public MotorControllingSubsystem() {
+    m_frontLeft = new CANSparkMax(1, MotorType.kBrushless);
+    m_backLeft = new CANSparkMax(2, MotorType.kBrushless);
+    m_frontRight = new CANSparkMax(3, MotorType.kBrushless);
+    m_backLeft = new CANSparkMax(4, MotorType.kBrushless);
+    m_frontLeft.setInverted(true);
+    m_backLeft.setInverted(true);
+    m_backLeft.follow(m_frontLeft);
+    m_backRight.follow(m_frontRight);
+    m_drive = new DifferentialDrive(m_frontLeft, m_frontRight);
+    m_leftStick = new Joystick(0);
+    m_rightStick = new Joystick(1);
+  }
+  public void tankDrive(double leftSpeed, double rightSpeed){
+    m_drive.tankDrive(leftSpeed, rightSpeed);
+  }
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    m_drive.tankDrive(m_leftStick.getY(), m_rightStick.getY());
   }
 }
